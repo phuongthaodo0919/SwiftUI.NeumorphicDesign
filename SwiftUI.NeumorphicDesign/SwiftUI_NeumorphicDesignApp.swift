06350371a420efc6b865d12fb3cc02da6ebee280
Salm Do <thaodo.usa@gmail.com>
2023-04-29 18:12:36 -0500

    first commit

---
+++ SwiftUI.NeumorphicDesign/SwiftUI_NeumorphicDesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_NeumorphicDesignApp.swift
+//  SwiftUI.NeumorphicDesign
+//
+//  Created by Salmdo on 4/29/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_NeumorphicDesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
